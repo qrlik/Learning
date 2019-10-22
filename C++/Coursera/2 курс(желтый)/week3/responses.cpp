f1a6a11358e42f5ed2dcb8df4c9e6f8a1b1df0bb
qrlik <54934287+qrlik@users.noreply.github.com>
2019-10-22 17:54:58 +0300

    Create responses.cpp

---
+++ C++/Coursera/2 курс(желтый)/week3/responses.cpp
@@ -0,0 +1,75 @@
+#include "responses.h"
+
+BusesForStopResponse::BusesForStopResponse()
+{ // по умолчанию остановки нет
+	ss << "No stop";
+}
+
+BusesForStopResponse::BusesForStopResponse(const std::vector<std::string>& buses)
+{ // список маршрутов через данную остановку
+	for (const auto& bus : buses)
+	{
+		ss << bus << " ";
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const BusesForStopResponse& r)
+{
+	return os << r.ss.str();
+}
+
+StopsForBusResponse::StopsForBusResponse()
+{ // по умолчанию маршрута нет
+	ss << "No bus";
+}
+
+StopsForBusResponse::StopsForBusResponse(const std::string& bus, const std::vector<std::string>& stops, const std::map<std::string, std::vector<std::string>>& stops_to_buses)
+{ // для маршрута список остановок и для каждой остановки список других маршрутов, проходящих через неё
+	for (const std::string& stop : stops)
+	{
+		ss << "Stop " << stop << ": ";
+		if (stops_to_buses.at(stop).size() == 1)
+		{
+			ss << "no interchange";
+		}
+		else
+		{
+			for (const std::string& other_bus : stops_to_buses.at(stop))
+			{
+				if (bus != other_bus)
+				{
+					ss << other_bus << " ";
+				}
+			}
+		}
+		ss << std::endl;
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const StopsForBusResponse& r)
+{
+	return os << r.ss.str();
+}
+
+AllBusesResponse::AllBusesResponse()
+{ // по умолчанию маршрутов нет
+	ss << "No buses";
+}
+
+AllBusesResponse::AllBusesResponse(const std::map<std::string, std::vector<std::string>> buses_to_stops)
+{ // все маршруты и их список остановок
+	for (const auto& bus_item : buses_to_stops)
+	{
+		ss << "Bus " << bus_item.first << ": ";
+		for (const std::string& stop : bus_item.second)
+		{
+			ss << stop << " ";
+		}
+		ss << std::endl;
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, const AllBusesResponse& r)
+{
+	return os << r.ss.str();
+}
