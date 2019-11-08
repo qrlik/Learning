6cee4f003bef798ec5a54f10f65deac90b2c9468
qrlik <54934287+qrlik@users.noreply.github.com>
2019-11-08 12:19:32 +0300

    Create main.cpp

---
+++ C++/Coursera/2 курс(желтый)/week6/main.cpp
@@ -0,0 +1,74 @@
+#include "database.h"
+#include "date.h"
+#include "condition_parser.h"
+#include "node.h"
+#include "test_runner.h"
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+template<typename T1, typename T2> std::ostream& operator<<(std::ostream& os, const std::pair<T1, T2>& p)
+{ // шаблонная функция печати пары
+	return os << p.first << ' ' << p.second;
+}
+
+void TestAll();
+
+int main() {
+	TestAll();
+
+	Database db;
+
+	for (string line; getline(cin, line); ) {
+		istringstream is(line);
+
+		string command;
+		is >> command;
+		if (command == "Add") {
+			const auto date = ParseDate(is);
+			const auto event = ParseEvent(is);
+			db.Add(date, event);
+		}
+		else if (command == "Print") {
+			db.Print(cout);
+		}
+		else if (command == "Del") {
+			auto condition = ParseCondition(is);
+			auto predicate = [condition](const Date& date, const string& event) {
+				return condition->Evaluate(date, event);
+			};
+			int count = db.RemoveIf(predicate);
+			cout << "Removed " << count << " entries" << endl;
+		}
+		else if (command == "Find") {
+			auto condition = ParseCondition(is);
+			auto predicate = [condition](const Date& date, const string& event) {
+				return condition->Evaluate(date, event);
+			};
+
+			const auto entries = db.FindIf(predicate);
+			for (const auto& entry : entries) {
+				cout << entry << endl;
+			}
+			cout << "Found " << entries.size() << " entries" << endl;
+		}
+		else if (command == "Last") {
+			try {
+				cout << db.Last(ParseDate(is)) << endl;
+			}
+			catch (invalid_argument&) {
+				cout << "No entries" << endl;
+			}
+		}
+		else if (command.empty()) {
+			continue;
+		}
+		else {
+			throw logic_error("Unknown command: " + command);
+		}
+	}
+
+	return 0;
+}
