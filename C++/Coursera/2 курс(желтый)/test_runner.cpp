7b2f95a1eced8650350a982922d86e08c87bb592
qrlik <54934287+qrlik@users.noreply.github.com>
2019-11-08 16:15:18 +0300

    Update test_runner.cpp

--- C++/Coursera/2 курс(желтый)/test_runner.cpp
+++ C++/Coursera/2 курс(желтый)/test_runner.cpp
@@ -5,25 +5,6 @@ void Assert(bool b, const std::string& hint) // проверка на логич
 	AssertEqual(b, true, hint);
 }
 
-template <class TestFunc> void TestRunner::RunTest(TestFunc func, const std::string& test_name) // шаблонный запуск тестирующей функции
-{
-	try
-	{
-		func();
-		std::cerr << test_name << " OK" << std::endl;
-	}
-	catch (std::exception & e)
-	{
-		++fail_count;
-		std::cerr << test_name << " fail: " << e.what() << std::endl;
-	}
-	catch (...)
-	{
-		++fail_count;
-		std::cerr << "Unknown exception caught" << std::endl;
-	}
-}
-
 TestRunner::~TestRunner() // проверка на верность тестов
 {
 	if (fail_count > 0)
