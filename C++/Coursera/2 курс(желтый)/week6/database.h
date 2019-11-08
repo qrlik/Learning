1d1551eb7876bc7b421395979374f27f82475519
qrlik <54934287+qrlik@users.noreply.github.com>
2019-11-08 12:19:20 +0300

    Update database.h

--- C++/Coursera/2 курс(желтый)/week6/database.h
+++ C++/Coursera/2 курс(желтый)/week6/database.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "date.h"
+#include "Events.h"
 #include <map>
 #include <set>
 #include <vector>
@@ -9,14 +10,37 @@
 class Database
 {
 private:
-	std::map<Date, std::set<std::string>> m_data; // храним уникальные значение
-	std::map<Date, std::vector<std::set<std::string>::const_iterator>> m_order; // храним порядок
+	std::map<Date, Events> m_data; // словарь событий
 public:
 	void Add(const Date& date, const std::string& event); // метод добавления события по дате
+	template <typename Func> size_t RemoveIf(Func Predicate) // удаление по предикату
+	{
+		size_t result = 0;
+
+		for (auto it = m_data.begin(); it != m_data.end(); ) // pair - Date, Events
+		{
+			result += it->second.RemoveIf([=](const std::string& event) { return Predicate(it->first, event); });
+
+			it = (it->second.getAll().empty()) ? m_data.erase(it) : next(it); // если удалились все события удаляем дату
+		}
+
+		return result;
+	}
+	template <typename Func> std::vector<std::pair<const Date&, const std::string&>> FindIf(Func Predicate) const // поиск по предикату
+	{
+		std::vector<std::pair<const Date&, const std::string&>> result;
+		for (const auto& [date, events] : m_data)
+		{ // итерируемся по датам
+			for (const auto& event : events.getAll())
+			{ // итерируемся по событиям даты
+				if (Predicate(date, event)) // если данные удовлетворяют предикату, заносим ссылки в результат
+				{
+					result.push_back({ date, event });
+				}
+			}
+		}
+		return result;
+	}
 	std::ostream& Print(std::ostream& os) const; // печать всей базы
-	size_t RemoveIf(const std::function<bool(const Date&, const std::string&)> f); // удаление по предикату
-	std::vector<std::pair<const Date&, const std::string&>> FindIf(std::function<bool(const Date&, const std::string&)> f) const; // поиск по предикату
 	std::string Last(const Date& date) const; // последнее событие к дате
 };
-
-std::string ParseEvent(std::istream& is);
