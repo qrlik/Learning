65d186cfb323c7d33b09966c773274e812a43601
qrlik <54934287+qrlik@users.noreply.github.com>
2019-12-06 18:43:29 +0300

    Update noupdateparallel.cpp

--- C++/Coursera/3 курс(красный)/week6/noupdateparallel.cpp
+++ C++/Coursera/3 курс(красный)/week6/noupdateparallel.cpp
@@ -1,8 +1,11 @@
 #include "search_server.h"
-#include "iterator_range.h"
 
 #include <algorithm>
-#include <array>
+
+//vector<string> SplitLine(const string& line) {
+//	istringstream words_input(line);
+//	return { istream_iterator<string>(words_input), istream_iterator<string>() };
+//}
 
 vector<string> SplitLine(const string& line) // разбить строку на слова
 {
@@ -26,8 +29,8 @@ void Database::ExploreLine(vector<string> doc_words, size_t doc_id)
 		DocsHits& word_data = letter_words[move(word)];
 		auto [it, is_insert] = word_data.id_index.insert({ doc_id, word_data.id_hits.size() }); // вставляем doc_id и предполагаемый index в id_hits
 		if (is_insert)
-		{ // если такого документа не было
-			word_data.id_hits.push_back({doc_id, 0}); // Добавляем пару doc_id , hitcount
+		{ // если такого документа в слове еще не было
+			word_data.id_hits.push_back({ doc_id, 0 }); // Добавляем пару doc_id , hitcount
 		}
 		++word_data.id_hits[it->second].second; // увеличиваем hitcount для нового/старого документа
 	}
@@ -64,7 +67,7 @@ void SearchServer::UpdateDocumentBase(istream& document_input) // обновле
 
 void SearchServer::AddQueriesStream(istream& query_input, ostream& search_results_output) const
 {
-	vector<pair<size_t, int>> docid_count(data.size()); // счетчик hitcount для всех документов <hitcount, docid>
+	vector<pair<size_t, size_t>> docid_count(data.size(), make_pair(0, 0)); // счетчик hitcount для всех документов <docid, hitcount>
 	const size_t relevant_size = min(static_cast<size_t>(5), data.size());
 
 	for (string current_query; getline(query_input, current_query); ) // получили запрос
@@ -77,8 +80,8 @@ void SearchServer::AddQueriesStream(istream& query_input, ostream& search_result
 			{ // проверяем есть ли слово в базе
 				for (auto [docid, hits] : data.Lookup(word)) // забираем информацию из id_hits
 				{
-					docid_count[docid].first += hits;
-					docid_count[docid].second = docid;
+					docid_count[docid].first = docid;
+					docid_count[docid].second += hits;
 				}
 			}
 			catch (out_of_range&)
@@ -87,24 +90,33 @@ void SearchServer::AddQueriesStream(istream& query_input, ostream& search_result
 			}
 		}
 
-		auto lambda = [](pair<size_t, int> lhs, pair<size_t, int> rhs)
+		//auto lambda = [](pair<size_t, int> lhs, pair<size_t, int> rhs)
+		//{
+		//	lhs.second = -lhs.second;
+		//	rhs.second = -rhs.second;
+		//	return lhs > rhs;
+		//};
+
+		auto lambda = [](pair<size_t, size_t> lhs, pair<size_t, size_t> rhs)
 		{
-			lhs.second = -lhs.second;
-			rhs.second = -rhs.second;
-			return lhs > rhs;
+			int64_t lhs_docid = lhs.first;
+			auto lhs_hit_count = lhs.second;
+			int64_t rhs_docid = rhs.first;
+			auto rhs_hit_count = rhs.second;
+			return make_pair(lhs_hit_count, -lhs_docid) > make_pair(rhs_hit_count, -rhs_docid);
 		};
 
 		partial_sort(docid_count.begin(), docid_count.begin() + relevant_size, docid_count.end(), lambda); // получаем первые 5 релевантных документов
 
 		search_results_output << current_query << ':'; // для пустого поиска выведет только [текст запроса]:
-		for (size_t i = 0; i < relevant_size && docid_count[i].first != 0; ++i) // выводим до 5 самых релевантных документов
+		for (size_t i = 0; i < relevant_size && docid_count[i].second != 0; ++i) // выводим до 5 самых релевантных документов c ненулевой релевантностью
 		{
 			search_results_output << " {"
-				<< "docid: " << docid_count[i].second << ", "
-				<< "hitcount: " << docid_count[i].first << '}';
+				<< "docid: " << docid_count[i].first << ", "
+				<< "hitcount: " << docid_count[i].second << '}';
 		}
 		search_results_output << '\n';
 
-		docid_count.assign(data.size(), make_pair(0, 0));
+		docid_count.assign(data.size(), make_pair(0, 0)); // обнуляем значения в векторе
 	}
 }
