c994ae462d8b32ce0eb9c43ad8e95e705dce8fb8
qrlik <54934287+qrlik@users.noreply.github.com>
2019-12-06 16:22:59 +0300

    Update noupdateparallel.h

--- C++/Coursera/3 курс(красный)/week6/noupdateparallel.h
+++ C++/Coursera/3 курс(красный)/week6/noupdateparallel.h
@@ -9,23 +9,27 @@ using namespace std;
 
 class Database
 { // класс данных для поисковой системы
-	map<string, map<size_t, size_t>> data_words; // слово - документы и их hitcount для данного слова (word (K) -> docid (V/K) -> hitcount(V))
-	size_t id_count = 0;
-
-	void ExploreLine(vector<string> doc_words, size_t doc_id);
+	struct DocsHits
+	{ // структура для быстрого итерирования в AddQueriesStream
+		vector<pair<size_t, size_t>> id_hits; // вектор документов для слова (индекс - hitcount)
+		map<size_t, size_t> id_index; // словарь для поиска документов (id - индекс в векторе)
+	};
+	map<string, DocsHits> letter_words; // словарь релевантности документов для данного слова (word (K) -> docid (V/K) -> hitcount(V))
+	size_t id_count;
+	void ExploreLine(vector<string> doc_words, size_t doc_id); // добавление слов из документа в базу
 public:
-	void Add(const string& document);
-	const map<size_t, size_t>& Lookup(const string& word) const;
+	Database();
+	void Add(const string& document); // добавить строку-документ в базу
+	const vector<pair<size_t, size_t>>& Lookup(const string& word) const; // словарь релевантности документов для слова
+	size_t size() const;
 };
 
 class SearchServer
 { // класс поисковой системы
 	Database data;
-	ostringstream AddQueriesStreamSingleThread(vector<string> buffer) const;
 public:
 	SearchServer() = default;
 	explicit SearchServer(istream& document_input);
-	void UpdateDocumentBase(istream& document_input);
-	void ConstructRelevant(const map<size_t, vector<size_t>>& hitcount_count, ostream& search_results_output, size_t amount) const;
-	void AddQueriesStream(istream& query_input, ostream& search_results_output) const;
+	void UpdateDocumentBase(istream& document_input); // обновить базу документов
+	void AddQueriesStream(istream& query_input, ostream& search_results_output) const; // обработать запросы
 };
