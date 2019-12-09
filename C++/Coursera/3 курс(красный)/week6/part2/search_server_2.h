7f9fa9c96b5f8e98cf488d2c39ffb3f6fe12bae5
qrlik <54934287+qrlik@users.noreply.github.com>
2019-12-09 18:07:19 +0300

    Create search_server_2.h

---
+++ C++/Coursera/3 курс(красный)/week6/part2/search_server_2.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <future>
+
+#include "synchronized.h"
+
+using namespace std;
+
+class Database
+{ // класс данных для поисковой системы
+	struct DocsHits
+	{ // структура для быстрого итерирования в AddQueriesStream
+		vector<pair<size_t, size_t>> id_hits; // вектор документов для слова (id - hitcount)
+		map<size_t, size_t> id_index; // словарь для поиска документов (id - индекс в векторе)
+	};
+	map<string, DocsHits> letter_words; // словарь релевантности документов для слов
+	size_t id_count; // количество документов в базе
+public:
+	Database();
+	void Add(const string& document); // добавить строку-документ в базу
+	const vector<pair<size_t, size_t>>& Lookup(const string& word) const; // словарь релевантности документов для слова
+	size_t size() const; // количество документов в базе
+};
+
+class SearchServer
+{ // класс поисковой системы
+	mutex dmutex;
+	Database data; // Database о оболочке с синхронным доступом
+
+	Synchronized<size_t> search_count; // счетчик потоков в поиске
+
+	vector<future<void>> update_futures;
+	vector<future<void>> search_futures;
+
+	void UpdateDocumentBaseThread(istream& document_input);
+	void AddQueriesStreamThread(istream& query_input, ostream& search_results_output);
+public:
+	SearchServer();
+	explicit SearchServer(istream& document_input);
+	void UpdateDocumentBase(istream& document_input); // обновить базу документов
+	void AddQueriesStream(istream& query_input, ostream& search_results_output); // обработать запросы
+};
