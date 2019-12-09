eba601972a99ba3ad5ab11843347004eac8ecd0c
qrlik <54934287+qrlik@users.noreply.github.com>
2019-12-09 14:17:48 +0300

    Update parallel.cpp

--- C++/Coursera/3 курс(красный)/week6/parallel.cpp
+++ C++/Coursera/3 курс(красный)/week6/parallel.cpp
@@ -1,64 +1,41 @@
 #include "search_server.h"
-#include "iterator_range.h"
 
-#include <future>
+#include <algorithm>
 #include <sstream>
-#include <functional>
+#include <iterator>
 
-vector<string> SplitLine(const string& line) // разбить строку на слова
+vector<string> SplitLine(const string& line)
 {
-	vector<string> result;
-	for (size_t not_space = line.find_first_not_of(' '); not_space != line.npos; )
-	{
-		size_t space = line.find_first_of(' ', not_space); // находим первый пробел (конец слова)
-		result.push_back(line.substr(not_space, space - not_space)); // заносим слово в вектор
-		not_space = line.find_first_not_of(' ', space); // ищем начало следующего слова
-	}
-	return result;
+	istringstream words_input(line);
+	return { istream_iterator<string>(words_input), istream_iterator<string>() };
 }
 
 // реализация Database
-Database::Database() :alphabet(25), id_count(0) {} // вектор для распараллеливания доступа по первой букве (алфавит из 25 букв)
-
-const Database& Database::operator=(Database&& other) noexcept
-{
-	for (size_t i = 0; i < 25; ++i)
-	{
-		alphabet[i].letter_words = move(other.alphabet[i].letter_words);
-	}
-	id_count = other.id_count;
-	other.id_count = 0;
-	return *this;
-}
-
-size_t Database::WordToIndex(const string& word) const
-{
-	return tolower(word[0]) - 'a';
-}
-
-void Database::ExploreLine(vector<string> doc_words, size_t doc_id)
-{
-	for (string& word : doc_words)
-	{
-		LetterAccess& letter = alphabet[WordToIndex(word)];
-		lock_guard letter_guard(letter.letter_mutex); // синхронизируем доступ к словарю по первой букве
-		++letter.letter_words[move(word)][doc_id]; // перемещаем слова в словарь,
-	}
-}
+Database::Database() :id_count(0) {}
 
 void Database::Add(const string& document) // получает объект и обновляет данные
 {
-	size_t current_id;
+	for (string& word : SplitLine(document))
 	{
-		lock_guard id_guard(id_mutex);  // синхронизируем счетчик документов
-		current_id = id_count++;
+		DocsHits& word_data = letter_words[move(word)];
+		auto [it, is_insert] = word_data.id_index.insert({ id_count, word_data.id_hits.size() }); // вставляем doc_id и предполагаемый index в id_hits
+		if (is_insert)
+		{ // если такого документа в слове еще не было
+			word_data.id_hits.push_back({ id_count, 0 }); // Добавляем пару doc_id , hitcount
+		}
+		++word_data.id_hits[it->second].second; // увеличиваем hitcount для нового/старого документа
 	}
-	ExploreLine(SplitLine(document), current_id); // разбиваем документ и обновляем data для текущего id
+	++id_count;
 }
 
-const map<size_t, size_t>& Database::Lookup(const string& word) const
+const vector<pair<size_t, size_t>>& Database::Lookup(const string& word) const
 {
-	return alphabet[WordToIndex(word)].letter_words.at(word);
+	return letter_words.at(word).id_hits;
+}
+
+size_t Database::size() const
+{
+	return id_count;
 }
 
 // реализация SearchServer
@@ -67,98 +44,84 @@ SearchServer::SearchServer(istream& document_input)
 	UpdateDocumentBase(document_input);
 }
 
-void SearchServer::UpdateDocumentBaseSingleThread(Database& new_data, vector<string> document_input)
-{
-	for (const string& current_document : document_input)
-	{ // одного копирования в SplitLine не избежать, поэтому дальше можно не move
-		new_data.Add(current_document);
-	}
-}
-// ДУБЛИРОВАНИЕ КОДА
-void SearchServer::UpdateDocumentBase(istream& document_input) // обновление базы документов (максимум 5*10^4 штук)
+void SearchServer::UpdateDocumentBaseThread(istream& document_input) // обновление базы документов (максимум 5*10^4 штук)
 {
 	Database new_data;
-	vector<future<void>> futures;
-	const size_t buffer_size = 2500;
-	vector<string> buffer;
-	buffer.reserve(buffer_size);
-
-	while (document_input)
+	for (string current_document; getline(document_input, current_document); ) // конструируем новый Database без mutex
 	{
-		string query;
-		for (size_t i = 0; i < buffer_size && getline(document_input, query); ++i)
+		if (!current_document.empty())
 		{
-			buffer.push_back(move(query));
+			new_data.Add(current_document);
 		}
-		futures.push_back(async(&SearchServer::UpdateDocumentBaseSingleThread, this, ref(new_data), move(buffer))); // отправляем в поток
-		buffer.reserve(buffer_size);
 	}
-
-	for (auto& f : futures)
-	{
-		f.get(); // получаем результаты
-	}
-
-	data = move(new_data);
+	data.GetAccess().ref_to_value = move(new_data); // получили доступ к данным и заменили
 }
 
-void SearchServer::ConstructRelevant(const map<size_t, vector<size_t>>& hitcount_count, ostream& search_results_output, size_t amount) const
+void SearchServer::UpdateDocumentBase(istream& document_input) // обновление базы документов (максимум 5*10^4 штук)
 {
-	if (!hitcount_count.empty())
-	{ // т.к 0 hitcount быть не может, поэтому проверяем что были ли вообще совпадения в базе
-		auto highest_hitcount = hitcount_count.rbegin(); // hitcount - vector<docid>
-		for (size_t i = 0; i < amount && highest_hitcount != hitcount_count.rend(); ++highest_hitcount) // выводим до 5 самых релевантных документов
-		{
-			for (size_t j = 0; i < amount && j < highest_hitcount->second.size(); ++i, ++j)
-			{
-				search_results_output << " {"
-					<< "docid: " << highest_hitcount->second[j] << ", "
-					<< "hitcount: " << highest_hitcount->first << '}';
-			}
-		}
-	}
-	search_results_output << '\n';
+	update_futures.push_back(async(&SearchServer::UpdateDocumentBaseThread, this, ref(document_input)));
 }
 
-ostringstream SearchServer::AddQueriesStreamSingleThread(vector<string> query_input) const
+ostringstream SearchServer::AddQueriesStreamSingleThread(vector<string> query_input)
 {
 	ostringstream search_results_output;
+	vector<pair<size_t, int>> docid_count; // счетчик hitcount для всех документов <hitcount, docid>
+	size_t relevant_size;
+	
 	for (const string& current_query : query_input) // получили запрос
 	{
 		vector<string> query_words = SplitLine(current_query); //вектор слов из запроса
 
-		map<size_t, size_t> docid_count; // счетчик hitcount
-		for (const auto& word : query_words) // для каждого слова в запросе
 		{
-			try
-			{ // проверяем есть ли слово в базе
-				for (auto [docid, hitcount] : data.Lookup(word)) // проходим по всем документам где есть это слово
+			auto data_access = data.GetAccess();  // получили доступ к данным
+			const Database& data_ref = data_access.ref_to_value;
+
+			docid_count.assign(data_ref.size(), make_pair(0, 0)); // обнуляем значения в векторе
+			relevant_size = min(static_cast<size_t>(5), docid_count.size());
+
+			for (const auto& word : query_words) // для каждого слова в запросе
+			{
+				try
+				{ // проверяем есть ли слово в базе
+					for (auto [docid, hits] : data_ref.Lookup(word)) // забираем информацию из id_hits
+					{
+						docid_count[docid].first += hits;
+						docid_count[docid].second = docid;
+					}
+				}
+				catch (out_of_range&)
 				{
-					docid_count[docid] += hitcount; // прибавляем hitcount для каждого
+					continue;
 				}
 			}
-			catch (out_of_range&)
-			{
-				continue;
-			}
 		}
 
-		map<size_t, vector<size_t>> hitcount_count; // hitcount - вектор документов (вектор сразу отсортирован по возврастанию)
-		for (auto [docid, hitcount] : docid_count) // проходим по всем документам
+		auto lambda = [](pair<size_t, int> lhs, pair<size_t, int> rhs)
 		{
-			hitcount_count[hitcount].push_back(docid); // обновляем hitcount
-		}
+			lhs.second = -lhs.second;
+			rhs.second = -rhs.second;
+			return lhs > rhs;
+		};
+
+		partial_sort(docid_count.begin(), docid_count.begin() + relevant_size, docid_count.end(), lambda); // получаем первые 5 релевантных документов
 
 		search_results_output << current_query << ':'; // для пустого поиска выведет только [текст запроса]:
-		ConstructRelevant(hitcount_count, search_results_output, 5); // отправить в результат до N самых релевантных документов
+		for (size_t i = 0; i < relevant_size && docid_count[i].first != 0; ++i) // выводим до 5 самых релевантных документов c ненулевой релевантностью
+		{
+			search_results_output << " {"
+				<< "docid: " << docid_count[i].second << ", "
+				<< "hitcount: " << docid_count[i].first << '}';
+		}
+		search_results_output << '\n';
 	}
+
 	return search_results_output;
 }
-// ДУБЛИРОВАНИЕ КОДА
-void SearchServer::AddQueriesStream(istream& query_input, ostream& search_results_output) const
+
+void SearchServer::AddQueriesStream(istream& query_input, ostream& search_results_output)
 {
 	vector<future<ostringstream>> futures;
-	const size_t buffer_size = 25000;
+	const size_t buffer_size = 4000;
 	vector<string> buffer;
 	buffer.reserve(buffer_size);
 
