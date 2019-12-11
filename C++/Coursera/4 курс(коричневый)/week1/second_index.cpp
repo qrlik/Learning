c33352c9b2bec38e0fb10b565afced48abf3e5be
qrlik <54934287+qrlik@users.noreply.github.com>
2019-12-11 18:47:55 +0300

    Update second_index.cpp

--- C++/Coursera/4 курс(коричневый)/week1/second_index.cpp
+++ C++/Coursera/4 курс(коричневый)/week1/second_index.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 #include <unordered_map>
+#include <list>
 
 using namespace std;
 
@@ -20,18 +21,23 @@ struct SecondIndexIt
 { // структура для хранения указателя и итераторов, для быстрой работы функций
     multimap<int, const Record*>::iterator timestamp;
     multimap<int, const Record*>::iterator karma;
-    multimap<string, const Record*>::iterator user;
+    list<const Record*>::iterator user;
+};
+
+struct IdData
+{
+    Record record;
+    SecondIndexIt iters;
 };
 
 // Реализуйте этот класс
 class Database
 {
-    unordered_map<string, Record> data; // id - Records
-    unordered_map<string, SecondIndexIt> ids; // словарь по id с быстрым доступом к данным
-    multimap<int, const Record*> timestamp_si; // вторичный индекс для timestamp
-    multimap<int, const Record*> karma_si; // вторичный индекс для karma
-    multimap<string, const Record*> user_si; // вторичный индекс для user_si
-    template<typename C, typename T, typename Callback> void RangeBy(const C& cont, const T& begin, const T& end, Callback callback) const
+    unordered_map<string, IdData> data;             // id - Records
+    multimap<int, const Record*> timestamp_si;      // вторичный индекс для timestamp
+    multimap<int, const Record*> karma_si;          // вторичный индекс для karma
+    unordered_map<string, list<const Record*>> user_si;   // вторичный индекс для user_si
+    template<typename T, typename Callback> void RangeBy(const multimap<int, const Record*>& cont, const T& begin, const T& end, Callback callback) const
     { // итерация по [begin,end] пока Callback возвращает true
         for (auto start = cont.lower_bound(begin); start != cont.upper_bound(end); ++start)
         {
@@ -45,15 +51,14 @@ class Database
 public:
     bool Put(const Record& record) // Операция Put должна возвращать true, если вставка удалась, и false, если вставка не удалась, потому что в базе данных уже есть запись с таким же id.
     {// В последнем случае состояние базы данных не должно меняться
-        auto [it, is_insert] = data.emplace(record.id, record); // пробуем вставить пару
+        auto [it, is_insert] = data.insert({ record.id, {record} }); // пробуем вставить пару
         if (is_insert)
-        { // вставка произошла, нужно обновить данные
-        	const Record* ptr = &it->second;
-            auto time_it = timestamp_si.emplace(record.timestamp, ptr); // обновляем индексы
-            auto karma_it = karma_si.emplace(record.karma, ptr);
-            auto user_it = user_si.emplace(record.user, ptr);
-            SecondIndexIt tmp { time_it, karma_it, user_it };
-            ids.emplace(record.id, move(tmp));
+        { // вставка произошла, нужно добавить данные в индексы и обновить в SecondIndexIt
+            const Record* ptr = &it->second.record;
+            auto time_it = timestamp_si.emplace(ptr->timestamp, ptr);
+            auto karma_it = karma_si.emplace(ptr->karma, ptr);
+            user_si[ptr->user].push_front(ptr);
+            it->second.iters = { time_it , karma_it, user_si[ptr->user].begin() };
         }
         return is_insert;
     }
@@ -62,7 +67,7 @@ public:
     {
         if (auto it = data.find(id); it != data.end())
         {
-            return &it->second;
+            return &it->second.record;
         }
         else
         {
@@ -72,17 +77,16 @@ public:
 
     bool Erase(const string& id) // Операция Erase должна возвращать true, если удалось удалить элемент с заданным id, и false, если элемент с заданным id не был найден.
     { // В последнем случае состояние базы данных не должно меняться
-        bool result = data.erase(id) != 0;
-        if (result)
-        { // если удаление произошло
-            auto old = ids.find(id);
-            const SecondIndexIt& iters= old->second;
+        if (auto search = data.find(id); search != data.end())
+        {
+            const SecondIndexIt& iters = search->second.iters;
             timestamp_si.erase(iters.timestamp);
             karma_si.erase(iters.karma);
-            user_si.erase(iters.user);
-            ids.erase(old);
+            user_si[search->second.record.user].erase(iters.user);
+            data.erase(search);
+            return true;
         }
-        return result;
+        return false;
     }
 
     // Подразумевается, что callback должен возвращать true, если требуется продолжить обход найденных записей, и false в противном случае.
@@ -100,7 +104,16 @@ public:
 
     template <typename Callback> void AllByUser(const string& user, Callback callback) const
     {
-        RangeBy(user_si, user, user, callback);
+        if (auto search = user_si.find(user); search != user_si.end())
+        {
+            for (auto ptr : search->second)
+            {
+                if (!callback(*ptr))
+                {
+                    break;
+                }
+            }
+        }
     }
 };
 
