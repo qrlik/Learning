a47cbe888d4291e7f25f3f2faeda94951c7e17f2
qrlik <54934287+qrlik@users.noreply.github.com>
2020-01-15 10:43:16 +0300

    Update trim2.cpp

--- C++/tasks/trim2.cpp
+++ C++/tasks/trim2.cpp
@@ -1,37 +1,37 @@
-#include <string>
-#include <cstring>
-using namespace std;
-
 /*
 Напишите функцию, которая обрезает пробелы в конце переданной ей строки.
 Функция должна быть написана в расчёте на работу с очень длинными строками с очень большим количеством пробелов, оптимизирована по количеству обращений к памяти.
 Сигнатура: void TrimRight( char *s );
 */
 
-// ~880мс на строке из 10^9 пробелов
+#include <string>
+#include <cstring>
+
+// на строке из 10^9 пробелов ~1000мс ~850мс(O2 оптимизация)
 
 void TrimRight(char* s) {
 	if (!s) {
 		return;
 	}
 
-	size_t len = strlen(s);
-	int mask = *reinterpret_cast<int*>(string(4, ' ').data());
-	int* new_ptr = reinterpret_cast<int*>(s + len % 4);
+	size_t length = strlen(s);
+	using step = int;
+	step space_mask = *reinterpret_cast<step*>(std::string(sizeof(step), ' ').data());
+	step* ptr = reinterpret_cast<step*>(s + length % sizeof(step));
 
-	for (size_t i = len / 4; i > 0; --i, len -= 4) {
-		if (new_ptr[i - 1] != mask) {
+	for (size_t i = length / sizeof(step); i > 0; --i, length -= sizeof(step)) {
+		if (ptr[i - 1] != space_mask) {
 			break;
 		}
 	}
 
-	for (; len > 0; --len) {
-		if (s[len - 1] != ' ') {
+	for (; length > 0; --length) { // хвостик
+		if (s[length - 1] != ' ') {
 			break;
 		}
 	}
 
-	s[len] = '\0';
+	s[length] = '\0';
 }
 
 int main()
