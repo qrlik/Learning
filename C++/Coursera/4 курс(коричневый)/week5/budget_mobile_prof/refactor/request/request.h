719f39eee75c0c37e06005ed50e5b81f93b21098
qrlik <54934287+qrlik@users.noreply.github.com>
2020-01-23 22:34:34 +0300

    Update request.h

--- C++/Coursera/4 курс(коричневый)/week5/budget_mobile_prof/refactor/request/request.h
+++ C++/Coursera/4 курс(коричневый)/week5/budget_mobile_prof/refactor/request/request.h
@@ -1,19 +1,94 @@
 #pragma once
-#include <vector>
+#include <unordered_map>
+#include <string_view>
+#include <optional>
+#include <memory>
+#include <iostream>
 
-#include "date.h"
-#include "parsers.h"
-#include "mentor_algorithm.h"
+#include "budget.h"
 
-static const Date START_DATE = Date::FromString("2000-01-01");
-static const Date END_DATE = Date::FromString("2100-01-01");
-static const size_t DAY_COUNT = ComputeDaysDiff(END_DATE, START_DATE);
-
-size_t ComputeDayIndex(const Date& date);
-IndexSegment MakeDateSegment(const Date& date_from, const Date& date_to);
-
-class BudgetManager : public SummingSegmentTree<MoneyState, BulkLinearUpdater> {
-public:
-	BudgetManager() : SummingSegmentTree(DAY_COUNT) {}
+enum class Type {
+	COMPUTE_INCOME,
+	EARN,
+	SPEND,
+	PAY_TAX
 };
 
+const std::unordered_map<std::string_view, Type> STR_TO_REQUEST_TYPE = {
+	{"ComputeIncome", Type::COMPUTE_INCOME},
+	{"Earn", Type::EARN},
+	{"Spend", Type::SPEND},
+	{"PayTax", Type::PAY_TAX}
+};
+
+std::optional<Type> ConvertRequestTypeFromString(std::string_view str);
+
+struct Request;
+using RequestHolder = std::unique_ptr<Request>;
+struct Request
+{
+	const Type type;
+
+	explicit Request(Type type) : type(type) {}
+	virtual void ParseFrom(std::string_view input) = 0;
+	static RequestHolder Create(Type type);
+	virtual ~Request() = default;
+};
+
+template <typename ResultType>
+struct ReadRequest : Request {
+	using Request::Request;
+	virtual ResultType Process(const BudgetManager& manager) const = 0;
+};
+
+struct ModifyRequest : Request {
+	using Request::Request;
+	virtual void Process(BudgetManager& manager) const = 0;
+};
+
+struct ComputeIncomeRequest : ReadRequest<double> {
+	ComputeIncomeRequest() : ReadRequest(Type::COMPUTE_INCOME) {}
+	void ParseFrom(std::string_view input) override;
+	double Process(const BudgetManager& manager) const override;
+
+	Date date_from = START_DATE;
+	Date date_to = START_DATE;
+};
+
+template <int SIGN>
+struct AddMoneyRequest : ModifyRequest {
+	static_assert(SIGN == -1 || SIGN == 1);
+
+	AddMoneyRequest() : ModifyRequest(SIGN == 1 ? Type::EARN : Type::SPEND) {}
+	void ParseFrom(std::string_view input) override {
+		date_from = Date::FromString(ReadToken(input));
+		date_to = Date::FromString(ReadToken(input));
+		value = ConvertToInt(input);
+	}
+
+	void Process(BudgetManager& manager) const override {
+		const auto date_segment = MakeDateSegment(date_from, date_to);
+		const double daily_value = value * 1.0 / date_segment.length();
+		const MoneyState daily_change = (SIGN == 1) ? MoneyState{ .earned = daily_value } : MoneyState{ .spend = daily_value };
+		manager.AddBulkOperation(date_segment, BulkMoneyAdder{ daily_change });
+	}
+
+	Date date_from = START_DATE;
+	Date date_to = START_DATE;
+	size_t value = 0;
+};
+
+struct PayTaxRequest : ModifyRequest {
+	PayTaxRequest() : ModifyRequest(Type::PAY_TAX) {}
+	void ParseFrom(std::string_view input) override;
+	void Process(BudgetManager& manager) const override;
+
+	Date date_from = START_DATE;
+	Date date_to = START_DATE;
+	uint8_t percentage = 0;
+};
+
+RequestHolder ParseRequest(std::string_view request_str);
+std::vector<RequestHolder> ReadRequests(std::istream& in_stream = std::cin);
+std::vector<double> ProcessRequests(const std::vector<RequestHolder>& requests);
+void PrintResponses(const std::vector<double>& responses, std::ostream& stream = std::cout);
