25a047d308c3a8396f13deaba8f65e2ee9855c21
qrlik <54934287+qrlik@users.noreply.github.com>
2020-01-28 20:30:31 +0300

    Update bus_manager.h

--- C++/Coursera/4 курс(коричневый)/week6/partA/bus_manager/bus_manager.h
+++ C++/Coursera/4 курс(коричневый)/week6/partA/bus_manager/bus_manager.h
@@ -7,23 +7,23 @@
 
 class BusManager
 {
-	struct StopInfo {
+	struct Stop_info {
 		double latitude; // широта
 		double longitude; // долгота
-		double Length(const StopInfo& other) const;
+		double Length(const Stop_info& other) const;
 	};
 
-	struct BusInfo {
+	struct Bus_info {
 		std::vector<std::string_view> stops;
 		double length;
 		size_t unique_stops;
 	};
 	double CalculateLength(const std::vector<std::string_view>& stops) const;
 
-	std::unordered_map<std::string, StopInfo> stop_data;
-	std::unordered_map<std::string, BusInfo> bus_data;
+	std::unordered_map<std::string, Stop_info> stop_data;
+	std::unordered_map<std::string, Bus_info> bus_data;
 public:
-	void AddStop(std::string name, StopInfo info);
+	void AddStop(std::string name, Stop_info info);
 	void AddBus(std::string name, const std::vector<std::string>& stops);
 	std::string BusInfo(std::string name) const;
 };
