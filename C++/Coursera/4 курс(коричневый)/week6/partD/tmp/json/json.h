b0752a3758443716e258a982e9d73a53327d755c
qrlik <54934287+qrlik@users.noreply.github.com>
2020-02-09 12:48:16 +0300

    Create json.h

---
+++ C++/Coursera/4 курс(коричневый)/week6/partD/tmp/json/json.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <istream>
+#include <map>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace Json {
+
+    class Node : std::variant<std::vector<Node>,
+        std::map<std::string, Node>,
+        double, bool,
+        std::string> {
+    public:
+        using variant::variant;
+
+        const auto& AsArray() const {
+            return std::get<std::vector<Node>>(*this);
+        }
+        const auto& AsMap() const {
+            return std::get<std::map<std::string, Node>>(*this);
+        }
+        double AsDouble() const {
+            return std::get<double>(*this);
+        }
+        const auto& AsString() const {
+            return std::get<std::string>(*this);
+        }
+        bool AsBool() const {
+            return std::get<bool>(*this);
+        }
+    };
+
+    class Document {
+    public:
+        explicit Document(Node root);
+
+        const Node& GetRoot() const;
+
+    private:
+        Node root;
+    };
+
+    Document Load(std::istream& input);
+
+}
