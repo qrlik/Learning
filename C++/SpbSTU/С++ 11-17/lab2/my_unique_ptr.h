028dfe8a796bafc40a71c9efa566394b96610b8d
qrlik <54934287+qrlik@users.noreply.github.com>
2020-03-18 16:46:02 +0300

    Create my_unique_ptr.h

---
+++ C++/SpbSTU/С++ 11-17/lab2/my_unique_ptr.h
@@ -0,0 +1,55 @@
+#pragma once
+
+template<typename T>
+class MyUniquePTR {
+	T* ptr{};
+public:
+	MyUniquePTR() = default;
+	explicit MyUniquePTR(T* other) : ptr(other) {}
+	explicit MyUniquePTR(T value) : ptr(new T(std::move(value))) {}
+	~MyUniquePTR() {
+		delete ptr;
+	}
+
+	MyUniquePTR(const MyUniquePTR&) = delete;
+	MyUniquePTR(MyUniquePTR&& other) noexcept : ptr(other.ptr) {
+		other.ptr = nullptr;
+	}
+
+	MyUniquePTR& operator=(const MyUniquePTR&) = delete;
+	MyUniquePTR& operator=(MyUniquePTR&& other) {
+		if (this != &other) {
+			MyUniquePTR(std::move(other)).swap(*this);
+		}
+		return *this;
+	}
+
+	void swap(MyUniquePTR& other) noexcept {
+		std::swap(ptr, other.ptr);
+	}
+
+	T* get() const noexcept {
+		return ptr;
+	}
+
+	void set(T value) {
+		if (!ptr) {
+			ptr = new T(std::move(value));
+		}
+		else {
+			*ptr = std::move(value);
+		}
+	}
+
+	T& operator*() const {
+		return *ptr;
+	}
+
+	T* operator->() const noexcept {
+		return ptr;
+	}
+
+	explicit operator bool() const noexcept {
+		return ptr != nullptr;
+	}
+};
